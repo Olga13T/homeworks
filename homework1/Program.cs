802cefdb3b50201e90779129e5075043b45d9e26
Helga <Helga13v@gmail.com>
2022-04-21 20:48:46 +0300

    file with ex_2-8

---
+++ homework1/Program.cs
@@ -0,0 +1,90 @@
+﻿// ЗАДАЧА 2. 
+// Написать программу, которая на вход принимает 2 числа
+//  и выдает какое число большее, какое меньшее.
+
+// int a, b;
+// a = 5; b = 7;
+
+// if (a > b)
+// {
+//     Console.Write("Max is: ");
+//     Console.WriteLine(a);
+// }
+// if (b > a)
+// {
+//     Console.Write("Max is: ");
+//     Console.WriteLine(b);
+// }
+// else 
+// {
+//     Console.Write("Max is a and b ");
+//     Console.WriteLine(a);
+// }
+
+// ///////////////////
+
+// Задача 4
+// На входе 3 числа
+// На выходе Max из 3 чисел
+
+// int a, b, c;
+// a = 22; b = 95; c = 18;
+// if (a > b & a> c)
+// {
+//     Console.Write("Max is: ");
+//     Console.WriteLine(a);
+// }
+// if (b > a & b > c)
+// {
+//     Console.Write("Max is: ");
+//     Console.WriteLine(b);
+// }
+// else 
+// {
+//     Console.Write("Max is: ");
+//     Console.WriteLine(c);
+// }
+
+////////////////////////
+
+// Задача 6
+// на входе число
+// на выходе: "четное оно или нет"?
+
+// int number = -91, numberChet;
+//  numberChet = number %2;
+
+// if (number < 0)
+// {
+//     number = -number;
+//     numberChet = number %2;
+// }
+// if(numberChet < 1)
+// {
+//     Console.Write($"Modul of number {number} is even");
+// }
+// else
+// {
+//     Console.Write($"Modul of number {number} is not even");
+// }
+// 
+/////////////////////
+
+// Задача 8
+// на входе число (N)
+// на выходе все четные от 1 до N
+
+
+int number, i;
+number = 26;
+i = 0;
+while(i < number) 
+   {
+     i +=2;
+   {
+    Console.Write($"{i}  ");
+    }
+   }   
+    
+    
+
