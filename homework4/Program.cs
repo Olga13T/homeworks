a1964f6b2135338e4765de14bc6e06934c982d60
Helga <Helga13v@gmail.com>
2022-04-23 21:49:21 +0300

    Домашнее задание к семинару 4- убрана закомментированность

--- homework4/Program.cs
+++ homework4/Program.cs
@@ -19,27 +19,27 @@ Console.WriteLine(result);
 // на входе - число 
 // на выходе - сумма цифр в числе
 
-// Console.WriteLine("Введите число:  ");
-// int a = Convert.ToInt32(Console.ReadLine());
-// int ostA = 0, sumA = 0, count = 0;
-// Console.WriteLine();
-// while (a > 0)
-// {
-//  ostA = a%10;
-//  a = a / 10;
-//  sumA = sumA + ostA;
-//  count++;
-// }
-//  Console.WriteLine(sumA);
+Console.WriteLine("Введите число:  ");
+int a = Convert.ToInt32(Console.ReadLine());
+int ostA = 0, sumA = 0, count = 0;
+Console.WriteLine();
+while (a > 0)
+{
+ ostA = a%10;
+ a = a / 10;
+ sumA = sumA + ostA;
+ count++;
+}
+ Console.WriteLine(sumA);
 
 
 //задача 29
 // программа задает массив из 8 элементов 
 // на выходе - выводит элементы на экран
 
-// int[] mass = new int[8];
-// for (int i = 0; i < mass.Length; i++)
-// {
-//     mass[i] = new Random(). Next(0, 100);
-//     Console.Write($"{mass[i]}  ");
-// }
+int[] mass = new int[8];
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = new Random(). Next(0, 100);
+    Console.Write($"{mass[i]}  ");
+}
